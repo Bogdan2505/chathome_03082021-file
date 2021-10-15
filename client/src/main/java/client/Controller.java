bf7bd7b898f62a893c0fa384ffe7929aab178710
Bogdan <bogbog680@gmail.com>
2021-10-15 19:29:38 +0300

    Initial commit

---
+++ client/src/main/java/client/Controller.java
@@ -0,0 +1,298 @@
+package client;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private ListView<String> clientList;
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField textField;
+    @FXML
+    private TextField loginField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private HBox authPanel;
+    @FXML
+    private HBox msgPanel;
+
+    private Socket socket;
+    private final int PORT = 8187;
+    private final String IP_ADDRESS = "localhost";
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private boolean authenticated;
+    private String nickname;
+    private Stage stage;
+    private Stage regStage;
+    private RegController regController;
+
+  //  private FileOutputStream outStrm;
+   // private File file = new File("history_chat.txt");
+  /*  private FileOutputStream outputStream;
+
+    {
+        try {
+            outputStream = new FileOutputStream("history_chat.txt", true );
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }*/
+    private BufferedWriter bufferedWriter;
+    {
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter("history_chat.txt", true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private BufferedWriter bufferedWriterUser;
+
+
+    ///Users/bogdan/IdeaProjects/chathome_03082021-5/users
+
+
+
+
+
+
+    //private FileInputStream inFile = new FileInputStream()
+
+
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+        authPanel.setVisible(!authenticated);
+        authPanel.setManaged(!authenticated);
+        msgPanel.setVisible(authenticated);
+        msgPanel.setManaged(authenticated);
+        clientList.setVisible(authenticated);
+        clientList.setManaged(authenticated);
+
+        if (!authenticated) {
+            nickname = "";
+        }
+        setTitle(nickname);
+        textArea.clear();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        Platform.runLater(() -> {
+            stage = (Stage) textField.getScene().getWindow();
+            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent event) {
+                    try {
+                        bufferedWriter.flush();
+                        bufferedWriterUser.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("bye");
+                    if (socket != null && !socket.isClosed()) {
+                        try {
+                            out.writeUTF("/end");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+        });
+        setAuthenticated(false);
+    }
+
+    private void connect() {
+        try {
+            socket = new Socket(IP_ADDRESS, PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            new Thread(() -> {
+                try {
+                    // цикл аутентификации
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                break;
+                            }
+                            if (str.startsWith("/authok")) {
+                                nickname = str.split("\\s")[1];
+                                File file = new File("users/" + nickname + ".txt");
+                                FileWriter writer = new FileWriter(file, true);
+                                writer.close();
+                                {
+                                    bufferedWriterUser = new BufferedWriter(writer);
+                                }
+
+
+                                setAuthenticated(true);
+                                break;
+                            }
+                            if (str.equals("/regok")) {
+                                regController.regResult("Регистрация прошла успешно");
+                            }
+                            if (str.equals("/regno")) {
+                                regController.regResult("Логин или никнейм уже заняты");
+                            }
+                        } else {
+                            textArea.appendText(str + "\n");
+                        }
+                    }
+                    // цикл работы
+                    while (authenticated) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/")) {
+                            if (str.equals("/end")) {
+                                break;
+                            }
+                            if (str.startsWith("/clientlist ")) {
+                                String[] token = str.split("\\s+");
+                                Platform.runLater(() -> {
+                                    clientList.getItems().clear();
+                                    for (int i = 1; i < token.length; i++) {
+                                        clientList.getItems().add(token[i]);
+                                    }
+                                });
+                            }
+                            //==============//
+                            if (str.startsWith("/yournickis ")) {
+                                nickname = str.split(" ")[1];
+                                setTitle(nickname);
+                            }
+                            //==============//
+                        } else {
+                            textArea.appendText(str + "\n");
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println("disconnected");
+                    setAuthenticated(false);
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void sendMsg(ActionEvent actionEvent) {
+
+        try {
+            bufferedWriter.write(nickname + ": " + textField.getText());
+            bufferedWriter.write("\n");
+
+           // outStrm.write(Integer.parseInt(textField.getText()));
+            out.writeUTF(textField.getText());
+            textField.clear();
+            textField.requestFocus();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        String login = loginField.getText().trim();
+        String password = passwordField.getText().trim();
+        String msg = String.format("/auth %s %s", login, password);
+
+        try {
+            out.writeUTF(msg);
+            passwordField.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setTitle(String nickname) {
+        Platform.runLater(() -> {
+            if (!nickname.equals("")) {
+                stage.setTitle(String.format("Home Chat[ %s ]", nickname));
+            } else {
+                stage.setTitle("Home Chat");
+            }
+        });
+    }
+
+    public void clientListClick(MouseEvent mouseEvent) {
+        String receiver = clientList.getSelectionModel().getSelectedItem();
+        textField.setText(String.format("/w %s ", receiver));
+    }
+
+    private void createRegWindow() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/reg.fxml"));
+            Parent root = fxmlLoader.load();
+            regStage = new Stage();
+            regStage.setTitle("Home Chat registration");
+            regStage.setScene(new Scene(root, 600, 400));
+            regController = fxmlLoader.getController();
+            regController.setController(this);
+
+            regStage.initStyle(StageStyle.UTILITY);
+            regStage.initModality(Modality.APPLICATION_MODAL);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showRegWindow(ActionEvent actionEvent) {
+        if (regStage == null) {
+            createRegWindow();
+        }
+        regStage.show();
+    }
+
+    public void registration(String login, String password, String nickname){
+        String msg = String.format("/reg %s %s %s",login, password, nickname);
+
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
